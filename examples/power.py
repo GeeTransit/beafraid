9110c0206cd32ea47fe72eb59bb9803ebd81a4ab
George Zhang <geetransit@gmail.com>
2021-09-06 11:22:08 -0400

    Use named constants in example

--- examples/power.py
+++ examples/power.py
@@ -25,37 +25,45 @@ def copya(origin, target, temp):
 def loopwhilenot(amount, code):
     return c(-amount) + loop(c(amount) + code + c(-amount)) + c(amount)
 
-def cont_switchon(current, delta, *cases, **kwargs):
-    cases = [at(-current, case) for case in cases]
-    return at(current, switchon(-current + delta, *cases, **kwargs))
+CONT_FRAMESIZE = 8
+[
+    CONT_BREAK, CONT_ZERO, CONT_ZERO_TEMP,
+    CONT_FUNC, CONT_FUNC_TEMP,
+    CONT_A, CONT_B, CONT_C,
+] = range(CONT_FRAMESIZE)
+CONT_BREAKON = -1
+
+def cont_switchon(delta, *cases, **kwargs):
+    cases = [at(-CONT_ZERO, case) for case in cases]
+    return at(CONT_ZERO, switchon(-CONT_ZERO + delta, *cases, **kwargs))
 
 # frame format: break, 0, 0, func, temp, a, b, c
 print("".join([
     # push func1(a=6)
     init([0, 0, 0, 1, 0, 6, 0, 0]),
     # loop while break!=-1
-    loopwhilenot(-1,
+    loopwhilenot(CONT_BREAKON,
         # check func using temp
-        cont_switchon(1, 3,
+        cont_switchon(CONT_FUNC,
             # default: save func0()
-            at(3, s(0)),
+            at(CONT_FUNC, s(0)),
             # func0(): set break=-1
-            c(-1),
+            c(CONT_BREAKON),
             # func1(a): save func0() and push func2(a=a)
-            at(3, s(0)) + at(8+3, s(2)) + movea(5, 8+5) + g(8),
+            at(CONT_FUNC, s(0)) + at(CONT_FRAMESIZE+CONT_FUNC, s(2)) + movea(CONT_A, CONT_FRAMESIZE+CONT_A) + g(CONT_FRAMESIZE),
             # func2(a): check a using b
-            cont_switchon(1, 5,
+            cont_switchon(CONT_A,
                 # if a!=0 save func3(a=a) and push func2(a=a-1)
-                at(3, s(3)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
+                at(CONT_FUNC, s(3)) + at(CONT_FRAMESIZE+CONT_FUNC, s(2)) + at(CONT_A, c(-1)) + copya(CONT_A, CONT_FRAMESIZE+CONT_A, CONT_C) + at(CONT_A, c(1)) + g(CONT_FRAMESIZE),
                 # else set a=1 and pop
-                at(5, s(1)) + g(-8),
+                at(CONT_A, s(1)) + g(-CONT_FRAMESIZE),
             ),
             # func3(a): save func4(a=a, b=^a) and push func2(a=a-1)
-            at(6, s(0)) + movea(8+5, 6) + at(3, s(4)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
+            at(CONT_B, s(0)) + movea(CONT_FRAMESIZE+CONT_A, CONT_B) + at(CONT_FUNC, s(4)) + at(CONT_FRAMESIZE+CONT_FUNC, s(2)) + at(CONT_A, c(-1)) + copya(CONT_A, CONT_FRAMESIZE+CONT_A, CONT_C) + at(CONT_A, c(1)) + g(CONT_FRAMESIZE),
             # func4(a, b): set a=b+^a and pop
-            at(7, s(0)) + movea(8+5, 7) + at(5, s(0)) + movea(6, 5) + movea(7, 5) + g(-8),
+            at(CONT_C, s(0)) + movea(CONT_FRAMESIZE+CONT_A, CONT_C) + at(CONT_A, s(0)) + movea(CONT_B, CONT_A) + movea(CONT_C, CONT_A) + g(-CONT_FRAMESIZE),
         ),
     ),
     # print ^a
-    at(8+5, "."),
+    at(CONT_FRAMESIZE+CONT_A, "."),
 ]))
