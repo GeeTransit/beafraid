eaf7e9863e56bb8061cf306a1245b3aaf99dcc70
George Zhang <geetransit@gmail.com>
2021-09-06 11:06:10 -0400

    Improve example

--- examples/power.py
+++ examples/power.py
@@ -25,6 +25,10 @@ def copya(origin, target, temp):
 def loopwhilenot(amount, code):
     return c(-amount) + loop(c(amount) + code + c(-amount)) + c(amount)
 
+def cont_switchon(current, delta, *cases, **kwargs):
+    cases = [at(-current, case) for case in cases]
+    return at(current, switchon(-current + delta, *cases, **kwargs))
+
 # frame format: break, 0, 0, func, temp, a, b, c
 print("".join([
     # push func1(a=6)
@@ -32,7 +36,7 @@ print("".join([
     # loop while break!=-1
     loopwhilenot(-1,
         # check func using temp
-        switchon(3,
+        cont_switchon(1, 3,
             # default: save func0()
             at(3, s(0)),
             # func0(): set break=-1
@@ -40,7 +44,7 @@ print("".join([
             # func1(a): save func0() and push func2(a=a)
             at(3, s(0)) + at(8+3, s(2)) + movea(5, 8+5) + g(8),
             # func2(a): check a using b
-            switchon(5,
+            cont_switchon(1, 5,
                 # if a!=0 save func3(a=a) and push func2(a=a-1)
                 at(3, s(3)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
                 # else set a=1 and pop
