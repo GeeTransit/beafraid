ba047e7379eb4805fdc47aceff2b7a055e366f85
George Zhang <geetransit@gmail.com>
2021-09-06 03:35:58 -0400

    Simplify switchon in power.py example

--- examples/power.py
+++ examples/power.py
@@ -13,7 +13,11 @@ print(chr(f(6)), end="")
 from beafraid import *
 
 def switchon(delta, *cases, **kwargs):
-    return g(delta) + switch(*[g(-delta) + case for case in cases], **kwargs)
+    return (
+        g(delta)
+        + switch(*[g(-delta) + case + g(1) for case in cases], **kwargs)
+        + g(-1)
+    )
 
 # frame format: break, 0, 0, func, temp, a, b, c
 print(
@@ -27,25 +31,24 @@ print(
         + switchon(
             3,
             # default: save func0()
-            at(3, s(0)) + g(1),
+            at(3, s(0)),
             # func0(): set break=-1
-            c(-1) + g(1),
+            c(-1),
             # func1(a): save func0() and push func2(a=a)
-            at(3, s(0)) + at(8+3, s(2)) + at(5, move(-5+8+5)) + g(8+1),
+            at(3, s(0)) + at(8+3, s(2)) + at(5, move(-5+8+5)) + g(8),
             # func2(a): check a using b
             switchon(
                 5,
                 # if a!=0 save func3(a=a) and push func2(a=a-1)
-                at(3, s(3)) + at(8+3, s(2)) + at(5, c(-1) + copy(2, -5+8+5) + c(1)) + g(8+1),
+                at(3, s(3)) + at(8+3, s(2)) + at(5, c(-1) + copy(2, -5+8+5) + c(1)) + g(8),
                 # else set a=1 and pop
-                at(5, s(1)) + g(-8+1),
+                at(5, s(1)) + g(-8),
             ),
             # func3(a): save func4(a=a, b=^a) and push func2(a=a-1)
-            at(6, s(0)) + at(8+5, move(-5-8+6)) + at(3, s(0) + c(4)) + at(8+3, s(2)) + at(5, c(-1) + copy(2, -5+8+5) + c(1)) + g(8+1),
+            at(6, s(0)) + at(8+5, move(-5-8+6)) + at(3, s(0) + c(4)) + at(8+3, s(2)) + at(5, c(-1) + copy(2, -5+8+5) + c(1)) + g(8),
             # func4(a, b): set a=b+^a and pop
-            at(7, s(0)) + at(8+5, move(-5-8+7)) + at(5, s(0)) + at(6, move(-6+5)) + at(7, move(-7+5)) + g(-8+1),
+            at(7, s(0)) + at(8+5, move(-5-8+7)) + at(5, s(0)) + at(6, move(-6+5)) + at(7, move(-7+5)) + g(-8),
         )
-        + g(-1)
         + c(1)
     )
     + c(-1)
