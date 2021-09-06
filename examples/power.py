2b97372c74b66447e2591a398e197a5dbd7fd1d7
George Zhang <geetransit@gmail.com>
2021-09-06 03:43:23 -0400

    Add movea and copya to power.py example

--- examples/power.py
+++ examples/power.py
@@ -19,6 +19,12 @@ def switchon(delta, *cases, **kwargs):
         + g(-1)
     )
 
+def movea(origin, target):
+    return at(origin, move(-origin + target))
+
+def copya(origin, target, temp):
+    return at(origin, copy(-origin + temp, -origin + target))
+
 # frame format: break, 0, 0, func, temp, a, b, c
 print(
     # push func1(a=6)
@@ -35,19 +41,19 @@ print(
             # func0(): set break=-1
             c(-1),
             # func1(a): save func0() and push func2(a=a)
-            at(3, s(0)) + at(8+3, s(2)) + at(5, move(-5+8+5)) + g(8),
+            at(3, s(0)) + at(8+3, s(2)) + movea(5, 8+5) + g(8),
             # func2(a): check a using b
             switchon(
                 5,
                 # if a!=0 save func3(a=a) and push func2(a=a-1)
-                at(3, s(3)) + at(8+3, s(2)) + at(5, c(-1) + copy(2, -5+8+5) + c(1)) + g(8),
+                at(3, s(3)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
                 # else set a=1 and pop
                 at(5, s(1)) + g(-8),
             ),
             # func3(a): save func4(a=a, b=^a) and push func2(a=a-1)
-            at(6, s(0)) + at(8+5, move(-5-8+6)) + at(3, s(0) + c(4)) + at(8+3, s(2)) + at(5, c(-1) + copy(2, -5+8+5) + c(1)) + g(8),
+            at(6, s(0)) + movea(8+5, 6) + at(3, s(0) + c(4)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
             # func4(a, b): set a=b+^a and pop
-            at(7, s(0)) + at(8+5, move(-5-8+7)) + at(5, s(0)) + at(6, move(-6+5)) + at(7, move(-7+5)) + g(-8),
+            at(7, s(0)) + movea(8+5, 7) + at(5, s(0)) + movea(6, 5) + movea(7, 5) + g(-8),
         )
         + c(1)
     )
