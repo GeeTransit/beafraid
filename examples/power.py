b2e97d61e75e83e2efd91906f7f3cbad004fc55a
George Zhang <geetransit@gmail.com>
2021-09-06 03:48:55 -0400

    Improve formatting of power.py example

--- examples/power.py
+++ examples/power.py
@@ -29,15 +29,13 @@ def loopwhilenot(amount, code):
     return c(-amount) + loop(c(amount) + code + c(-amount)) + c(amount)
 
 # frame format: break, 0, 0, func, temp, a, b, c
-print(
+print("".join([
     # push func1(a=6)
-    init([0, 0, 0, 1, 0, 6, 0, 0])
+    init([0, 0, 0, 1, 0, 6, 0, 0]),
     # loop while break!=-1
-    + loopwhilenot(
-        -1,
+    loopwhilenot(-1,
         # check func using temp
-        switchon(
-            3,
+        switchon(3,
             # default: save func0()
             at(3, s(0)),
             # func0(): set break=-1
@@ -45,19 +43,18 @@ print(
             # func1(a): save func0() and push func2(a=a)
             at(3, s(0)) + at(8+3, s(2)) + movea(5, 8+5) + g(8),
             # func2(a): check a using b
-            switchon(
-                5,
+            switchon(5,
                 # if a!=0 save func3(a=a) and push func2(a=a-1)
                 at(3, s(3)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
                 # else set a=1 and pop
                 at(5, s(1)) + g(-8),
             ),
             # func3(a): save func4(a=a, b=^a) and push func2(a=a-1)
-            at(6, s(0)) + movea(8+5, 6) + at(3, s(0) + c(4)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
+            at(6, s(0)) + movea(8+5, 6) + at(3, s(4)) + at(8+3, s(2)) + at(5, c(-1)) + copya(5, 8+5, 7) + at(5, c(1)) + g(8),
             # func4(a, b): set a=b+^a and pop
             at(7, s(0)) + movea(8+5, 7) + at(5, s(0)) + movea(6, 5) + movea(7, 5) + g(-8),
         ),
-    )
+    ),
     # print ^a
-    + at(8+5, ".")
-)
+    at(8+5, "."),
+]))
